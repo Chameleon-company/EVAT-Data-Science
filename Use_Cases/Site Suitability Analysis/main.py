2ec8d72b029d7e322f2f9635917f35830035078f
Hetti Arachchige Amantha Kalhara Kulathunga <s224093674@deakin.edu.au>
2026-05-07 14:29:37 +1000

    Refactored Site Suitability scoring outputs for dual-model API compatibility
    This update refactors the Site Suitability Analysis pipeline to ensure compatibility between both revised suitability scoring models and the FastAPI backend.
    
    Changes made:
    
    * Updated the suitability scoring notebook to export an API-ready dataset (`priority_sites_version2.csv`) and ('priority_sites_version1.csv) which are outputs from each scoring models.
    * Replaced the original `SiteScore` with the revised `NewSuitabilityScore`
    * Re-ranked sites using the revised suitability score
    * Removed intermediate scoring columns not required by the API
    * Standardised dataset headers and structure to match the format expected by `main.py`
    * Updated the FastAPI backend to load `priority_sites_version2.csv` as it is the most recent model

--- Use_Cases/Site Suitability Analysis/main.py
+++ Use_Cases/Site Suitability Analysis/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from geopy.distance import geodesic
 
 # Load the dataset
-df = pd.read_csv("priority_sites.csv")
+df = pd.read_csv("priority_sites_version2.csv")
 
 app = FastAPI(title="EVAT Site Suitability API")
 
